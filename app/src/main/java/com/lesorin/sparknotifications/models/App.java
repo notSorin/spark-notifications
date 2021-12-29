eedec6e1d83d9c7ba2fe2bee02c91ce17e075990
Sorin <dsorin95@gmail.com>
2021-12-29 20:48:49 +0100

    Code formatting.

--- app/src/main/java/com/lesorin/sparknotifications/models/App.java
+++ app/src/main/java/com/lesorin/sparknotifications/models/App.java
@@ -2,48 +2,56 @@ package com.lesorin.sparknotifications.models;
 
 import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
-
 import androidx.annotation.Nullable;
-
 import io.realm.RealmObject;
 import io.realm.annotations.Required;
 
-public class App extends RealmObject {
-
+public class App extends RealmObject
+{
     @Required
     private String packageName;
     private String name;
     private boolean enabled;
 
-    public String getPackageName() {
+    public String getPackageName()
+    {
         return packageName;
     }
 
-    public void setPackageName(String packageName) {
+    public void setPackageName(String packageName)
+    {
         this.packageName = packageName;
     }
 
-    public boolean getEnabled() {
+    public boolean getEnabled()
+    {
         return enabled;
     }
 
-    public void setEnabled(boolean enabled) {
+    public void setEnabled(boolean enabled)
+    {
         this.enabled = enabled;
     }
 
-    public String getName() {
+    public String getName()
+    {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(String name)
+    {
         this.name = name;
     }
 
     @Nullable
-    public static Drawable getIcon(App app, PackageManager packageManager) {
-        try {
+    public static Drawable getIcon(App app, PackageManager packageManager)
+    {
+        try
+        {
             return packageManager.getApplicationInfo(app.getPackageName(), 0).loadIcon(packageManager);
-        } catch (PackageManager.NameNotFoundException | NullPointerException | OutOfMemoryError e) {
+        }
+        catch(PackageManager.NameNotFoundException | NullPointerException | OutOfMemoryError e)
+        {
             return null;
         }
     }
--- app/src/main/java/com/lesorin/sparknotifications/models/RecentApp.java
+++ app/src/main/java/com/lesorin/sparknotifications/models/RecentApp.java
@@ -4,20 +4,15 @@ import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
-
-
-import com.lesorin.sparknotifications.R;
-
 import io.realm.RealmObject;
 import io.realm.annotations.Ignore;
 import io.realm.annotations.Required;
 
-public class RecentApp extends RealmObject {
-
+public class RecentApp extends RealmObject
+{
     @Required
     private String packageName;
     private long timestamp;
-
     @Ignore
     private boolean informationFetched;
     @Ignore
@@ -27,69 +22,90 @@ public class RecentApp extends RealmObject {
     @Ignore
     private Drawable icon;
 
-    public String getPackageName() {
+    public String getPackageName()
+    {
         return packageName;
     }
 
-    public void setPackageName(String packageName) {
+    public void setPackageName(String packageName)
+    {
         this.packageName = packageName;
     }
 
-    public long getTimestamp() {
+    public long getTimestamp()
+    {
         return timestamp;
     }
 
-    public void setTimestamp(long timestamp) {
+    public void setTimestamp(long timestamp)
+    {
         this.timestamp = timestamp;
     }
 
-    public boolean isInstalled() {
+    public boolean isInstalled()
+    {
         return installed;
     }
 
-    public void setInstalled(boolean installed) {
+    public void setInstalled(boolean installed)
+    {
         this.installed = installed;
     }
 
-    public String getName() {
+    public String getName()
+    {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(String name)
+    {
         this.name = name;
     }
 
-    public Drawable getIcon() {
+    public Drawable getIcon()
+    {
         return icon;
     }
 
-    public void setIcon(Drawable icon) {
+    public void setIcon(Drawable icon)
+    {
         this.icon = icon;
     }
 
-    public boolean isInformationFetched() {
+    public boolean isInformationFetched()
+    {
         return informationFetched;
     }
 
-    public void setInformationFetched(boolean informationFetched) {
+    public void setInformationFetched(boolean informationFetched)
+    {
         this.informationFetched = informationFetched;
     }
 
-    public static void fetchInformation(RecentApp app, Context context) {
-        if (app.isInformationFetched()) {
+    public static void fetchInformation(RecentApp app, Context context)
+    {
+        if(app.isInformationFetched())
+        {
             return;
         }
 
         PackageManager packageManager = context.getPackageManager();
-        try {
+
+        try
+        {
             ApplicationInfo applicationInfo = packageManager.getApplicationInfo(app.getPackageName(), 0);
+
             app.setInstalled(true);
-            app.setName((String) applicationInfo.loadLabel(packageManager));
+            app.setName((String)applicationInfo.loadLabel(packageManager));
             app.setIcon(applicationInfo.loadIcon(packageManager));
-        } catch (OutOfMemoryError e) {
+        }
+        catch(OutOfMemoryError e)
+        {
             //LoggerFactory.getLogger("App").warn("OutOfMemoryError: " + e);
             //app.setIcon(context.getResources().getDrawable(R.drawable.sym_def_app_icon));
-        } catch (PackageManager.NameNotFoundException e) {
+        }
+        catch(PackageManager.NameNotFoundException e)
+        {
             app.setInstalled(false);
         }
 
