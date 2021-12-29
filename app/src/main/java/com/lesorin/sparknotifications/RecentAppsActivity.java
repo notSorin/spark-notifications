aa05acf5451e81cdb7e24968e60c0ee7032bc6e9
Sorin <dsorin95@gmail.com>
2021-12-29 19:51:43 +0100

    Code formatting.

--- app/src/main/java/com/lesorin/sparknotifications/RecentAppsActivity.java
+++ app/src/main/java/com/lesorin/sparknotifications/RecentAppsActivity.java
@@ -3,32 +3,36 @@ package com.lesorin.sparknotifications;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.ListView;
-
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.lesorin.sparknotifications.adapters.RecentAppsAdapter;
 
-public class RecentAppsActivity extends AppCompatActivity {
-
+public class RecentAppsActivity extends AppCompatActivity
+{
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recent_apps);
 
-        if (getSupportActionBar() != null) {
+        if(getSupportActionBar() != null)
+        {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
         ListView listView = ((ListView) findViewById(R.id.recentAppsList));
+
         listView.setAdapter(new RecentAppsAdapter(this));
         listView.setEmptyView(findViewById(R.id.no_apps));
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(item.getItemId() == android.R.id.home)
+        {
             finish();
+
             return true;
         }
 
--- app/src/main/java/com/lesorin/sparknotifications/ScreenNotificationsActivity.java
+++ app/src/main/java/com/lesorin/sparknotifications/ScreenNotificationsActivity.java
@@ -2,18 +2,16 @@ package com.lesorin.sparknotifications;
 
 import android.content.Intent;
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.lesorin.sparknotifications.services.AppScanningService;
 
-public class ScreenNotificationsActivity extends AppCompatActivity {
-
+public class ScreenNotificationsActivity extends AppCompatActivity
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
-
         startService(new Intent(this, AppScanningService.class));
     }
 }
