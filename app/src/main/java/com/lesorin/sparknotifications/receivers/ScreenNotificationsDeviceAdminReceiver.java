c4289113ce39e33f50028fc65d2a366311fd5d21
Sorin <dsorin95@gmail.com>
2021-12-29 20:44:33 +0100

    Code formatting.

--- app/src/main/java/com/lesorin/sparknotifications/receivers/ScreenNotificationsDeviceAdminReceiver.java
+++ app/src/main/java/com/lesorin/sparknotifications/receivers/ScreenNotificationsDeviceAdminReceiver.java
@@ -6,31 +6,45 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 
-public class ScreenNotificationsDeviceAdminReceiver extends DeviceAdminReceiver {
+public class ScreenNotificationsDeviceAdminReceiver extends DeviceAdminReceiver
+{
+    @Override
+    public void onEnabled(Context context, Intent intent)
+    {
+    }
 
     @Override
-    public void onEnabled(Context context, Intent intent) {}
-
-    @Override
-    public CharSequence onDisableRequested(Context context, Intent intent) {
+    public CharSequence onDisableRequested(Context context, Intent intent)
+    {
         ComponentName deviceAdmin = new ComponentName(context, ScreenNotificationsDeviceAdminReceiver.class);
-        ((DevicePolicyManager) context.getSystemService(Context.DEVICE_POLICY_SERVICE)).removeActiveAdmin(deviceAdmin);
+
+        ((DevicePolicyManager)context.getSystemService(Context.DEVICE_POLICY_SERVICE)).removeActiveAdmin(deviceAdmin);
 
         return null;
     }
 
     @Override
-    public void onDisabled(Context context, Intent intent) {}
+    public void onDisabled(Context context, Intent intent)
+    {
+    }
 
     @Override
-    public void onPasswordChanged(Context context, Intent intent) {}
+    public void onPasswordChanged(Context context, Intent intent)
+    {
+    }
 
     @Override
-    public void onPasswordFailed(Context context, Intent intent) {}
+    public void onPasswordFailed(Context context, Intent intent)
+    {
+    }
 
     @Override
-    public void onPasswordSucceeded(Context context, Intent intent) {}
+    public void onPasswordSucceeded(Context context, Intent intent)
+    {
+    }
 
     @Override
-    public void onPasswordExpiring(Context context, Intent intent) {}
+    public void onPasswordExpiring(Context context, Intent intent)
+    {
+    }
 }
--- app/src/main/java/com/lesorin/sparknotifications/receivers/UserPresentReceiver.java
+++ app/src/main/java/com/lesorin/sparknotifications/receivers/UserPresentReceiver.java
@@ -3,15 +3,17 @@ package com.lesorin.sparknotifications.receivers;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-
 import com.lesorin.sparknotifications.helpers.NotificationServiceHelper;
 
-public class UserPresentReceiver extends BroadcastReceiver {
-
+public class UserPresentReceiver extends BroadcastReceiver
+{
     @Override
-    public void onReceive(Context context, Intent intent) {
-        if (NotificationServiceHelper.isServiceEnabled(context)) {
-            if (!NotificationServiceHelper.isServiceRunning(context)) {
+    public void onReceive(Context context, Intent intent)
+    {
+        if(NotificationServiceHelper.isServiceEnabled(context))
+        {
+            if(!NotificationServiceHelper.isServiceRunning(context))
+            {
                 //LoggerFactory.getLogger("UserPresentReceiver").error("Service is enabled, but not running");
             }
         }
