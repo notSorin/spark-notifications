a4bc94439cffc72a2c9392dd8e1719909d2b26ed
Sorin <dsorin95@gmail.com>
2022-01-03 03:22:00 +0100

    Separated function into smaller functions.

--- app/src/main/java/com/lesorin/sparknotifications/services/AppScanningService.java
+++ app/src/main/java/com/lesorin/sparknotifications/services/AppScanningService.java
@@ -10,6 +10,7 @@ import java.util.List;
 import io.realm.Realm;
 import io.realm.RealmResults;
 
+//TODO try to remove this as service and call the functions when needed.
 public class AppScanningService extends IntentService
 {
     public AppScanningService()
@@ -20,50 +21,110 @@ public class AppScanningService extends IntentService
     @Override
     protected void onHandleIntent(Intent intent)
     {
-        Realm realm = Realm.getDefaultInstance();
-        RealmResults<App> previousApps = realm.where(App.class).findAll();
-        ArrayList<String> previousAppPackages = new ArrayList<>();
+        ArrayList<String> previousAppPackages = getRealmAppPackages();
+        ArrayList<String> installedAppPackages = getInstalledAppPackages();
 
-        for(App app : previousApps)
-        {
-            previousAppPackages.add(app.getPackageName());
-        }
+        updateRealmApps(installedAppPackages);
+        previousAppPackages.removeAll(installedAppPackages);
+        removeAppsFromRealm(previousAppPackages);
+    }
+
+    /**
+     * Updates Realm with new apps from a list of app packages.
+     *
+     * @param appPackages A list of app packages to add to Realm.
+     */
+    private void updateRealmApps(ArrayList<String> appPackages)
+    {
+        PackageManager packageManager = getPackageManager();
+        Realm realm = Realm.getDefaultInstance();
 
         realm.beginTransaction();
 
-        PackageManager packageManager = getPackageManager();
-        List<ApplicationInfo> applications = packageManager.getInstalledApplications(PackageManager.GET_META_DATA);
-
-        for(ApplicationInfo applicationInfo : applications)
+        for(String packageName : appPackages)
         {
-            if(!applicationInfo.enabled)
+            App app = realm.where(App.class).equalTo("packageName", packageName).findFirst();
+
+            if(app == null) //If the app isn't in Realm, create it.
             {
-                continue;
-            }
+                try
+                {
+                    ApplicationInfo appInfo = packageManager.getApplicationInfo(packageName, PackageManager.GET_META_DATA);
+                    app = realm.createObject(App.class);
 
-            App app = realm.where(App.class).equalTo("packageName", applicationInfo.packageName).findFirst();
-
-            if(app == null)
-            {
-                app = realm.createObject(App.class);
-            }
-
-            app.setPackageName(applicationInfo.packageName);
-            app.setName((String)applicationInfo.loadLabel(packageManager));
-            previousAppPackages.remove(applicationInfo.packageName);
-        }
-
-        for(String uninstalledAppPackage : previousAppPackages)
-        {
-            App uninstalledApp = realm.where(App.class).equalTo("packageName", uninstalledAppPackage).findFirst();
-
-            if(uninstalledApp != null)
-            {
-                uninstalledApp.deleteFromRealm();
+                    app.setPackageName(packageName);
+                    app.setName(appInfo.loadLabel(packageManager).toString());
+                }
+                catch(PackageManager.NameNotFoundException ignored)
+                {
+                }
             }
         }
 
         realm.commitTransaction();
         realm.close();
+    }
+
+    /**
+     * @return A list with all the packages of the apps currently installed on the device.
+     */
+    private ArrayList<String> getInstalledAppPackages()
+    {
+        ArrayList<String> installedAppPackages = new ArrayList<>();
+        List<ApplicationInfo> installedApps = getPackageManager().getInstalledApplications(PackageManager.GET_META_DATA);
+
+        for(ApplicationInfo appInfo : installedApps)
+        {
+            if(appInfo.enabled) //Disabled apps are considered uninstalled.
+            {
+                installedAppPackages.add(appInfo.packageName);
+            }
+        }
+
+        return installedAppPackages;
+    }
+
+    /**
+     * Removes apps from Realm that match the packages in previousAppPackages.
+     *
+     * @param appPackages A list with the app packages to remove from Realm.
+     */
+    private void removeAppsFromRealm(ArrayList<String> appPackages)
+    {
+        Realm realm = Realm.getDefaultInstance();
+
+        realm.beginTransaction();
+
+        //Loop each app package and remove it from Realm.
+        for(String appPackage : appPackages)
+        {
+            App app = realm.where(App.class).equalTo("packageName", appPackage).findFirst();
+
+            if(app != null)
+            {
+                app.deleteFromRealm();
+            }
+        }
+
+        realm.commitTransaction();
+    }
+
+    /**
+     * @return A list with all the app packages currently saved in Realm.
+     */
+    private ArrayList<String> getRealmAppPackages()
+    {
+        Realm realm = Realm.getDefaultInstance();
+        RealmResults<App> savedApps = realm.where(App.class).findAll();
+        ArrayList<String> appPackages = new ArrayList<>();
+
+        for(App app : savedApps)
+        {
+            appPackages.add(app.getPackageName());
+        }
+
+        realm.close();
+
+        return appPackages;
     }
 }
