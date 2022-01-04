7582443fc712e1dab75e6cbb9a15858fce97c35d
Sorin <dsorin95@gmail.com>
2022-01-04 01:44:43 +0100

    Changed activity name.

--- app/src/main/java/com/lesorin/sparknotifications/MainActivity.java
+++ app/src/main/java/com/lesorin/sparknotifications/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import com.lesorin.sparknotifications.services.AppScanningService;
 
-public class ScreenNotificationsActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity
 {
     @Override
     protected void onCreate(Bundle savedInstanceState)
