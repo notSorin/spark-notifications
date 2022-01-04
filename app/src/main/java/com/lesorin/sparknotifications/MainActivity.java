d974541ba4a56d096f14ee7c5ffe0ff533205488
Sorin <dsorin95@gmail.com>
2022-01-04 01:53:39 +0100

    Renamed xml file.

--- app/src/main/java/com/lesorin/sparknotifications/MainActivity.java
+++ app/src/main/java/com/lesorin/sparknotifications/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.settings);
+        setContentView(R.layout.main_activity_layout);
         startService(new Intent(this, AppScanningService.class));
     }
 }
