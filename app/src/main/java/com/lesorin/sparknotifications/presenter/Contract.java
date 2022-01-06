118d7439dfd92f05e2583630e6d41da8318b98ae
Sorin <dsorin95@gmail.com>
2022-01-06 22:44:05 +0100

    Created an MVP contract.

---
+++ app/src/main/java/com/lesorin/sparknotifications/presenter/Contract.java
@@ -0,0 +1,24 @@
+package com.lesorin.sparknotifications.presenter;
+
+public interface Contract
+{
+    interface PresenterView
+    {
+
+    }
+
+    interface PresenterModel
+    {
+
+    }
+
+    interface View
+    {
+
+    }
+
+    interface Model
+    {
+
+    }
+}
