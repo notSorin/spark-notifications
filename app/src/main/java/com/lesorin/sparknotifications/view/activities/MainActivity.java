38637bf4773d2716e115b2645247f6fa53a366fd
Sorin <dsorin95@gmail.com>
2022-01-09 22:23:07 +0100

    Added SettingsFragment reference for future use.

--- app/src/main/java/com/lesorin/sparknotifications/view/activities/MainActivity.java
+++ app/src/main/java/com/lesorin/sparknotifications/view/activities/MainActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.lesorin.sparknotifications.MainApplication;
 import com.lesorin.sparknotifications.R;
 import com.lesorin.sparknotifications.presenter.Contract;
+import com.lesorin.sparknotifications.view.fragments.SettingsFragment;
 
 public class MainActivity extends AppCompatActivity implements Contract.View
 {
     private Contract.PresenterView _presenter;
+    private SettingsFragment _settingsFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -16,6 +18,8 @@ public class MainActivity extends AppCompatActivity implements Contract.View
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ((MainApplication)getApplication()).activityChanged(this);
+
+        _settingsFragment = (SettingsFragment)getFragmentManager().findFragmentById(R.id.SettingsFragment);
     }
 
     @Override
