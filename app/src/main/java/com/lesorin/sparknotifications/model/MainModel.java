a67dbb546c859a3037ea946c8e0ff0949675d241
Sorin <dsorin95@gmail.com>
2022-01-09 21:47:28 +0100

    Added context and shared preferences to MainModel.

--- app/src/main/java/com/lesorin/sparknotifications/model/MainModel.java
+++ app/src/main/java/com/lesorin/sparknotifications/model/MainModel.java
@@ -1,16 +1,24 @@
 package com.lesorin.sparknotifications.model;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import com.lesorin.sparknotifications.presenter.Contract;
 import io.realm.Realm;
 
 public class MainModel implements Contract.Model
 {
+    private Context _context;
     private Contract.PresenterModel _presenter;
+    private SharedPreferences _preferences;
 
     public MainModel(Context context)
     {
-        Realm.init(context);
+        _context = context;
+        _preferences = PreferenceManager.getDefaultSharedPreferences(_context);
+
+        Realm.init(_context);
+
     }
 
     public void setPresenter(Contract.PresenterModel presenter)
